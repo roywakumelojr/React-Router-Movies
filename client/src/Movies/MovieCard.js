e0c476eea33d83951cdbe37659815ce6d17bea7a
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-09-17 17:47:56 -0400

    Part 2 and Stretch Goals Complete

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const MovieCard = props => {
-  return;
+  return (
+    <div className="movie-card">
+      <h2>{props.title}</h2>
+      <div className="movie-director">
+        Director: <em>{props.director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{props.metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+      
+      {props.stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+      <button className="save-button" onClick={props.saveMovie}>Save</button>
+    </div>
+  )
 };
 
 export default MovieCard;
